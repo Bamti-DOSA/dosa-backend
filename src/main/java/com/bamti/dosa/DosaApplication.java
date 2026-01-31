0fbfc9d7efbcd1e024f8930e35848620fdfbe9bb
whc9999 <whc3377@gmail.com>
2026-02-01 03:25:31 +0900

    [CHORE] 초기 프로젝트 셋업

---
+++ src/main/java/com/bamti/dosa/DosaApplication.java
@@ -0,0 +1,13 @@
+package com.bamti.dosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bamti/dosa/DosaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bamti.dosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
